b530b0be0af1a88839e2c3db55aef5f1ffd6c9a4
iamfinnz <ilham19ti@mahasiswa.pcr.ac.id>
2022-06-29 22:06:35 +0700

    Refactor reached, story and footer

---
+++ components/molecules/ReachedItem/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface ReachedItemProps {
+    value: string;
+    title: string;
+}
+export default function ReachedItem(props: ReachedItemProps) {
+  const { value, title } = props;
+  return (
+    <div className="me-lg-35">
+      <p className="text-4xl text-lg-start text-center color-palette-1 fw-bold m-0">{value}</p>
+      <p className="text-lg text-lg-start text-center color-palette-2 m-0">{title}</p>
+    </div>
+  );
+}
